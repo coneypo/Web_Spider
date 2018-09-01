d2e7d2724e69e1b0105664dc904afd7c3286b567
coneypo <coneypo@gmail.com>
2018-09-01 14:30:28 +0800

    update the way to get proxies

--- get_news_from_ithome.py
+++ get_news_from_ithome.py
@@ -1,49 +1,83 @@
 # Author:   coneypo
 # Created:  08.31
+# Updated:  09.01
 # web spider for xxx.com
 
 from bs4 import BeautifulSoup
 import requests
 import random
 import datetime
+import csv
+import pandas as pd
+
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
 }
 
 
-# 设置代理
+# 从 xicidaili.com 爬取代理地址，存入本地csv中
 def get_proxy():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+    }
     proxy_url = 'http://www.xicidaili.com/nn/'
     resp = requests.get(proxy_url, headers=headers)
     soup = BeautifulSoup(resp.text, 'lxml')
     ips = soup.find_all('tr')
     proxy_list = []
-
     for i in range(1, len(ips)):
         ip_info = ips[i]
         tds = ip_info.find_all('td')
         proxy_list.append(tds[1].text + ':' + tds[2].text)
 
-    proxy = proxy_list[random.randint(0, len(proxy_list))]
+    if len(proxy_list) == 0:
+        print("可能被代理网站屏蔽了", '\n')
+    else:
+        print("拿到的代理个数:", len(proxy_list), '\n')
+        with open("proxies.csv", 'w', newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            for i in range(len(proxy_list)):
+                # print(proxy_list[i])
+                writer.writerow([proxy_list[i]])
 
-    print("Proxy:", proxy)
-    return proxy
+
+# 执行一次就可以了；
+# 执行一次可以更新本地代理地址；
+get_proxy()
+
+
+# 从存放代理的 csv 中随机获取一个代理
+def get_random_proxy():
+    column_names = ["proxy"]
+    proxy_csv = pd.read_csv("proxies.csv", names=column_names)
+
+    # 新建一个列表用来存储代理地址
+    proxy_arr = []
+
+    for i in range(len(proxy_csv["proxy"])):
+        proxy_arr.append(proxy_csv["proxy"][i])
+
+    # 随机选择一个代理
+    random_proxy = proxy_arr[random.randint(0, len(proxy_arr)-1)]
+
+    print("当前代理:", random_proxy)
+    return random_proxy
 
 
 # 爬取网站
 def get_news():
     html = "https://www.ithome.com/blog/"
-    resp = requests.get(html, headers=headers, proxies=get_proxy())
+    resp = requests.get(html, headers=headers, proxies=get_random_proxy())
     resp.encoding = 'utf-8'
 
     # 网页内容
     bsObj = BeautifulSoup(resp.content, "lxml")
-    #print(bsObj)
+    # print(bsObj)
 
     # 分析
     block = bsObj.find_all("div", {"class": "block"})
-    #print(block)
+    # print(block)
 
     current_titles = []
     current_times = []
@@ -66,6 +100,7 @@ def get_news():
     for i in range(len(current_times)):
         print(current_times[i], current_titles[i])
     return current_times, current_titles
+
 
 get_news()
 
--- set_proxy.py
+++ set_proxy.py
@@ -1,12 +1,19 @@
 # Author:   coneypo
 # Created:  08.31
+# Updated:  09.01
 # set proxy
+# save to csv
 
 from bs4 import BeautifulSoup
 import requests
+import csv
+import pandas as pd
 import random
 
+path_proxy_csv = "proxies.csv"
 
+
+# 从 xicidaili.com 爬取代理地址，存入本地csv中
 def get_proxy():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
@@ -16,16 +23,40 @@ def get_proxy():
     soup = BeautifulSoup(resp.text, 'lxml')
     ips = soup.find_all('tr')
     proxy_list = []
-
     for i in range(1, len(ips)):
         ip_info = ips[i]
         tds = ip_info.find_all('td')
         proxy_list.append(tds[1].text + ':' + tds[2].text)
 
-    proxy = proxy_list[random.randint(0, len(proxy_list))]
+    if len(proxy_list) == 0:
+        print("可能被屏蔽了")
+    else:
+        print("拿到的代理个数:", len(proxy_list))
+        with open(path_proxy_csv, 'w', newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            for i in range(len(proxy_list)):
+                print(proxy_list[i])
+                writer.writerow([proxy_list[i]])
 
-    print(proxy)
-    return proxy
-get_proxy()
+
+# get_proxy()
 
 
+# 从代理的 csv 中随机获取一个代理
+def get_random_proxy():
+    column_names = ["proxy"]
+    proxy_csv = pd.read_csv("proxies.csv", names=column_names)
+
+    # 新建一个列表用来存储代理地址
+    proxy_arr = []
+
+    for i in range(len(proxy_csv["proxy"])):
+        proxy_arr.append(proxy_csv["proxy"][i])
+
+    # 随机选择一个代理
+    random_proxy = proxy_arr[random.randint(0, len(proxy_arr) - 1)]
+
+    print(random_proxy)
+    return random_proxy
+
+# get_random_proxy()
